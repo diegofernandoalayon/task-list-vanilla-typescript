14b2746888596aaeb625a1508a1c68a3632e4f2e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-24 10:17:57 -0500

    guardando las tareas en localStorage

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import './style.css'
 
 const taskForm = document.querySelector<HTMLFormElement>('#taskForm') // para indicar que el un elemento formulario
 
+interface Task {
+  title: string
+  description: string
+}
+
+let tasks: Task[] = []
+
 taskForm?.addEventListener('submit', (event) => {
   event.preventDefault()
   const title = taskForm['title'] as unknown as HTMLInputElement // para indicar que el un unknown y luege un elemento input de HTML
   
   const description = taskForm['description'] as unknown as HTMLTextAreaElement // para indicar que el elemento a seleccionar es un text area de HTML
 
-  console.log(title.value, description.value)
+  tasks.push({
+    title : title.value,
+    description: description.value
+  })
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
 })
