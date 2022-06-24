d8879ca1b4e5052ae208eb845f6b51f4369a021e
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-24 10:01:34 -0500

    seleccionando los elementos del formulario con typeScript

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const taskForm = document.querySelector<HTMLFormElement>('#taskForm') // para indicar que el un elemento formulario
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-`
+taskForm?.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const title = taskForm['title'] as unknown as HTMLInputElement // para indicar que el un unknown y luege un elemento input de HTML
+  
+  const description = taskForm['description'] as unknown as HTMLTextAreaElement // para indicar que el elemento a seleccionar es un text area de HTML
+
+  console.log(title.value, description.value)
+})
