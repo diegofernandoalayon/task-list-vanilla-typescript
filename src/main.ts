34e6f9608d86c61e2d54a1885b19d6a5224d3ecd
Diego Fernando Alayon <dfar.41@gmail.com>
2022-06-24 11:57:24 -0500

    listando tareas,

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 
 const taskForm = document.querySelector<HTMLFormElement>('#taskForm') // para indicar que el un elemento formulario
 
+const tasksList = document.querySelector<HTMLDivElement>('#tasksList')
+
 interface Task {
   title: string
   description: string
@@ -9,6 +11,7 @@ interface Task {
 
 let tasks: Task[] = []
 
+// creando tareas
 taskForm?.addEventListener('submit', (event) => {
   event.preventDefault()
   const title = taskForm['title'] as unknown as HTMLInputElement // para indicar que el un unknown y luege un elemento input de HTML
@@ -22,4 +25,46 @@ taskForm?.addEventListener('submit', (event) => {
 
   localStorage.setItem('tasks', JSON.stringify(tasks))
 
+  renderTasks(tasks) // para renderizar inmediatamente las tareas
+
+  taskForm.reset() // para resetear todo el formulario
+  title.focus() // para enfocar luego de guardar en la casilla del titulo
 })
+  //listando tareas
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  tasks = JSON.parse(localStorage.getItem('tasks') || '[]') // pedimos al localStorage y si no asignamos array vacios, esto dara valores a el array tasks
+  renderTasks(tasks)
+})
+
+function renderTasks(tasks: Task[]){
+  
+  tasksList!.innerHTML  = '' // ! para indicar que el elemento estara presente
+
+  tasks.forEach((task) => {
+    const taskElement = document.createElement('div')
+    taskElement.className = 'bg-gray-900 p-4 mb-1 rounded-lg hover:bg-gray-700 cursor:pointer'
+
+    const header = document.createElement('header')
+    header.className = 'flex justify-between'
+
+    const title = document.createElement('span')
+    title.innerText = task.title
+
+    const btnDelete = document.createElement('button')
+    btnDelete.className = 'bg-red-500 px-2 py-1 rounded-lg'
+    btnDelete.innerText = 'Delete'
+    header.append(title)
+    header.append(btnDelete) 
+
+    const description = document.createElement('span')
+    description.innerText = task.description
+
+
+    taskElement.append(header)
+    taskElement.append(description)
+
+    tasksList?.append(taskElement)
+  })
+}
